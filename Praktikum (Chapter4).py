913bc0d67b139d40f94ade39e4045d3c55693244
Nofia Nisa <nofiaanis29@gmail.com>
2020-10-17 23:14:24 +0700

    Initial commit

---
+++ Praktikum (Chapter4).py
@@ -0,0 +1,231 @@
+#Praktikum 1
+
+#nomor dua
+print(type(10))
+
+#nomor empat
+print(type(10.1918))
+
+#nomor enam
+print(type('Hello world'))
+
+#nomor delapan
+print(type(True))
+
+print()
+
+#Praktikum 2
+
+#nomor dua
+x=10
+print(type(x))
+y=20
+print(type(y))
+print(type(x+y))
+
+print()
+
+#nomor empat
+#Penjumlahan
+a = 10
+b = 10
+print(type(a+b))
+
+a = 10
+b = 0.1
+print(type(a+b))
+
+a = 0.1
+b = 10
+print(type(a+b))
+
+a = 0.1
+b = 0.2
+print(type(a+b))
+
+print()
+
+#Pengurangan
+a = 10
+b = 10
+print(type(a-b))
+
+a = 10
+b = 0.1
+print(type(a-b))
+
+a = 0.1
+b = 10
+print(type(a-b))
+
+a = 0.1
+b = 0.2
+print(type(a-b))
+
+print()
+
+#Perkalian
+a = 10
+b = 10
+print(type(a*b))
+
+a = 10
+b = 0.1
+print(type(a*b))
+
+a = 0.1
+b = 10
+print(type(a*b))
+
+a = 0.1
+b = 0.2
+print(type(a*b))
+
+print()
+
+#Pembagian
+a = 20
+b = 10
+print(type(a/b))
+
+a = 10
+b = 0.1
+print(type(a/b))
+
+a = 0.1
+b = 10
+print(type(a/b))
+
+a = 0.1
+b = 0.2
+print(type(a/b))
+
+print()
+
+#Pembagian Pangkat
+a = 10
+b = 10
+print(type(a//b))
+
+a = 10
+b = 0.1
+print(type(a//b))
+
+a = 0.1
+b = 10
+print(type(a//b))
+
+a = 0.1
+b = 0.2
+print(type(a//b))
+
+print()
+
+#Persentase
+a = 10
+b = 10
+print(type(a%b))
+
+a = 10
+b = 0.1
+print(type(a%b))
+
+a = 0.1
+b = 10
+print(type(a%b))
+
+a = 0.1
+b = 0.2
+print(type(a%b))
+
+print()
+
+#Perkalian Pangkat
+a = 10
+b = 10
+print(type(a**b))
+
+a = 10
+b = 0.1
+print(type(a**b))
+
+a = 0.1
+b = 10
+print(type(a**b))
+
+a = 0.1
+b = 0.2
+print(type(a**b))
+
+print()
+
+#nomor lima
+a=10
+p=y=x=z=a
+print(a)
+print(z)
+print(x)
+print(y)
+print(p)
+
+print()
+
+#Latihan
+
+#nomor satu
+#total tarif yang harus dibayar
+Tarif1=200000
+LamaTarif1=12
+Tarif2=10000
+LamaTarif=1
+JamMulai=6
+MenitMulai=0.0
+JamSelesai=23
+MenitSelesai=0.50
+LamaSewa=(JamSelesai-JamMulai)+(MenitSelesai-MenitMulai)
+SisaTarif2=(LamaSewa-LamaTarif1)
+Total=print(Tarif1+(SisaTarif2*Tarif2))
+
+print()
+
+#nomor dua
+#banyak bensin yang diperlukan
+JarakkotaAkeC=795
+KonsumsiBBM=1/12
+Bensin=print(KonsumsiBBM*JarakkotaAkeC)
+
+print()
+
+#nomor tiga
+#mengisi bensin
+Kapasitas=50
+Bensin=KonsumsiBBM*JarakkotaAkeC
+Pengisisan=print(Bensin/Kapasitas)
+
+print()
+
+#nomor empat
+#kedatangan Pak Amir
+JarakkotaAkeB=125
+Kecepatan1=62
+JarakkotaBkeC=256
+Kecepatan2=70
+Berangkat=6.0
+Istirahat=0.45
+WaktudarikotaAkeB=(JarakkotaAkeB/Kecepatan1)
+WaktudarikotaBkeC=(JarakkotaBkeC/Kecepatan2)
+Totallamawaktu=(WaktudarikotaAkeB+WaktudarikotaBkeC+Istirahat)
+SampaikekotaC=print(Berangkat+Totallamawaktu)
+
+print()
+
+#nomor lima
+BanyakMhsLaki=50
+BanyakMhsPerempuan=20
+print("Silahkan masukkan data laki:",BanyakMhsLaki)
+print("Silahkan masukkan data perempuan:",BanyakMhsPerempuan)
+a='*'
+DiagramAwalLaki=print("Grafik laki-laki:",a*BanyakMhsLaki)
+DiagramAwalPerempuan=print("Grafik perempuan:",a*BanyakMhsPerempuan)
+
+
